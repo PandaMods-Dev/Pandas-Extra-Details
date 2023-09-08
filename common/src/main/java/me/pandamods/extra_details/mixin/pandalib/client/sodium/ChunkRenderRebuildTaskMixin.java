82ddf490b849a89b1b3b8cf80dc4f170ae9085a4
Olive <Oliver.h.froberg@gmail.com>
2023-09-08 10:31:54 +0200

    Read changelog.md

--- common/src/main/java/me/pandamods/extra_details/mixin/pandalib/client/sodium/ChunkRenderRebuildTaskMixin.java
+++ common/src/main/java/me/pandamods/extra_details/mixin/pandalib/client/sodium/ChunkRenderRebuildTaskMixin.java
@@ -2,6 +2,7 @@ package me.pandamods.extra_details.mixin.pandalib.client.sodium;
 
 import me.jellysquid.mods.sodium.client.gl.compile.ChunkBuildContext;
 import me.jellysquid.mods.sodium.client.render.chunk.RenderSection;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildResult;
 import me.jellysquid.mods.sodium.client.render.chunk.tasks.ChunkRenderBuildTask;
 import me.jellysquid.mods.sodium.client.render.chunk.tasks.ChunkRenderRebuildTask;
 import me.jellysquid.mods.sodium.client.util.task.CancellationSource;
@@ -15,6 +16,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,7 +25,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ChunkRenderRebuildTask.class)
@@ -34,18 +39,11 @@ public abstract class ChunkRenderRebuildTaskMixin extends ChunkRenderBuildTask {
 
 	@Shadow @Final private ChunkRenderContext renderContext;
 
-	@Inject(method = "performBuild", at = @At("TAIL"), remap = false)
-    public void injectRenderPositions(ChunkBuildContext context, CancellationSource cancellationSource, CallbackInfoReturnable<ChunkBuildContext> cir) {
-        CompileResultsExtension extension = (CompileResultsExtension) cir.getReturnValue().data;
+	private Map<BlockPos, ClientBlock> clientBlockCache = new HashMap<>();
 
-        for (BlockPos pos : BlockPos.betweenClosed(this.render.getOriginX(), this.render.getOriginY(), this.render.getOriginZ(), this.render.getOriginX() + 16, this.render.getOriginY() + 16, this.render.getOriginZ() + 16)) {
-            BlockState state = context.cache.getWorldSlice().getBlockState(pos);
-            if (state.isAir())
-                continue;
-            if (BlockRendererDispatcher.shouldRender(state)) {
-                extension.getBlocks().add(pos.immutable());
-            }
-        }
+	@Inject(method = "performBuild", at = @At("RETURN"), remap = false)
+    public void injectRenderPositions(ChunkBuildContext context, CancellationSource cancellationSource, CallbackInfoReturnable<ChunkBuildResult> cir) {
+        CompileResultsExtension extension = (CompileResultsExtension) cir.getReturnValue().data;
 
 		for (BlockPos pos : BlockPos.betweenClosed(
 				this.render.getChunkPos().origin(),
@@ -56,15 +54,19 @@ public abstract class ChunkRenderRebuildTaskMixin extends ChunkRenderBuildTask {
 					continue;
 				ClientBlockProvider blockProvider = ClientBlockRegistry.get(state.getBlock());
 				if (blockProvider != null) {
-					List<ClientBlock> compiledBlocks = ((CompileResultsExtension) ).getBlocks().stream().filter(
+					List<ClientBlock> compiledBlocks = clientBlockCache.values().stream().filter(
 							clientBlock -> clientBlock.getBlockPos().equals(pos.immutable()) && clientBlock.getBlockState().is(state.getBlock())).toList();
 					if (compiledBlocks.isEmpty()) {
-						((CompileResultsExtension) (Object) compileResults).getBlocks().add(
-							blockProvider.create(pos.immutable(), state, Minecraft.getInstance().level));
+						ClientBlock block = blockProvider.create(pos.immutable(), state, Minecraft.getInstance().level);
+						extension.getBlocks().add(block);
+
+						clientBlockCache.put(block.getBlockPos(), block);
 					} else {
 						ClientBlock block = compiledBlocks.get(0);
 						block.setBlockState(state);
-						((CompileResultsExtension) (Object) compileResults).getBlocks().add(block);
+						extension.getBlocks().add(block);
+
+						clientBlockCache.put(block.getBlockPos(), block);
 					}
 				}
 			}
