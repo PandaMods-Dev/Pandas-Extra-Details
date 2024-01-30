f01b5ba6f36103eb89cfe36d500f7ff10c5f53d9
Olive <Oliver.h.froberg@gmail.com>
2024-01-30 13:22:35 +0100

    Testing performance when rendering hardcoded faces

---
+++ common/src/main/java/me/pandamods/extra_details/api/client/render/block/ClientBlockRenderDispatcher.java
@@ -0,0 +1,28 @@
+package me.pandamods.extra_details.api.client.render.block;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.CrashReport;
+import net.minecraft.CrashReportCategory;
+import net.minecraft.ReportedException;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ClientBlockRenderDispatcher {
+//	public static final Map<BlockPos, BlockRenderer> RENDERERS = new HashMap<>();
+	public static final List<BlockRenderer> RENDERERS = new ArrayList<>();
+
+	public static void render(List<BlockRenderer> renderers, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource) {
+		for (BlockRenderer renderer : renderers) {
+			renderer.render(poseStack, bufferSource);
+		}
+	}
+}
