c024287b356e28d5b52a4645f4821c54f5585317
The Panda Oliver <Oliver.h.froberg@gmail.com>
2024-07-09 00:57:33 +0200

    Add TrapDoorRenderer to ExtraDetails
    Added a new class, TrapDoorRenderer, to handle the rendering of trap doors. Updated ExtraDetails.java to include this new renderer in the BlockRendererRegistry. This boosts the performance and visual details of trap doors.

--- common/src/main/java/me/pandamods/extra_details/ExtraDetails.java
+++ common/src/main/java/me/pandamods/extra_details/ExtraDetails.java
@@ -6,6 +6,7 @@ import dev.architectury.registry.ReloadListenerRegistry;
 import me.pandamods.extra_details.api.render.BlockRendererRegistry;
 import me.pandamods.extra_details.client.renderer.DoorRenderer;
 import me.pandamods.extra_details.client.renderer.LeverRenderer;
+import me.pandamods.extra_details.client.renderer.TrapDoorRenderer;
 import me.pandamods.pandalib.client.render.PLInternalShaders;
 import me.pandamods.pandalib.resource.AssimpResources;
 import net.minecraft.resources.ResourceLocation;
@@ -24,8 +25,13 @@ public class ExtraDetails {
 		ReloadListenerRegistry.register(PackType.CLIENT_RESOURCES, new AssimpResources(), ID("assimp_loader"));
 
 		BlockRendererRegistry.register(new LeverRenderer(), Blocks.LEVER);
+
 		BlockRendererRegistry.register(new DoorRenderer(), Blocks.OAK_DOOR, Blocks.DARK_OAK_DOOR, Blocks.ACACIA_DOOR, Blocks.BAMBOO_DOOR, Blocks.BIRCH_DOOR,
 				Blocks.CHERRY_DOOR, Blocks.CRIMSON_DOOR, Blocks.WARPED_DOOR, Blocks.JUNGLE_DOOR, Blocks.SPRUCE_DOOR, Blocks.MANGROVE_DOOR, Blocks.IRON_DOOR);
+
+		BlockRendererRegistry.register(new TrapDoorRenderer(), Blocks.OAK_TRAPDOOR, Blocks.DARK_OAK_TRAPDOOR, Blocks.ACACIA_TRAPDOOR, Blocks.BAMBOO_TRAPDOOR,
+				Blocks.BIRCH_TRAPDOOR, Blocks.CHERRY_TRAPDOOR, Blocks.CRIMSON_TRAPDOOR, Blocks.WARPED_TRAPDOOR, Blocks.JUNGLE_TRAPDOOR, Blocks.SPRUCE_TRAPDOOR,
+				Blocks.MANGROVE_TRAPDOOR, Blocks.IRON_TRAPDOOR);
 	}
 
 	public static ResourceLocation ID(String path) {
---
+++ common/src/main/java/me/pandamods/extra_details/client/renderer/TrapDoorRenderer.java
@@ -0,0 +1,93 @@
+package me.pandamods.extra_details.client.renderer;
+
+import com.mojang.blaze3d.Blaze3D;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import me.pandamods.extra_details.ExtraDetails;
+import me.pandamods.extra_details.api.blockdata.BlockData;
+import me.pandamods.extra_details.api.render.MeshBlockRenderer;
+import me.pandamods.pandalib.client.animation.Animatable;
+import me.pandamods.pandalib.client.animation.AnimatableInstance;
+import me.pandamods.pandalib.client.animation.states.AnimationController;
+import me.pandamods.pandalib.client.animation.states.AnimationState;
+import me.pandamods.pandalib.client.animation.states.State;
+import me.pandamods.pandalib.resource.AssimpResources;
+import me.pandamods.pandalib.resource.Mesh;
+import me.pandamods.pandalib.utils.MatrixUtils;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.TrapDoorBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Half;
+
+public class TrapDoorRenderer implements MeshBlockRenderer<TrapDoorRenderer.TrapDoorData>, AnimationController<TrapDoorRenderer.TrapDoorData> {
+	private final Mesh topMesh = AssimpResources.getMesh(ExtraDetails.ID("assimp/meshes/block/trapdoor/trap_door_top.fbx"));
+	private final Mesh bottomMesh = AssimpResources.getMesh(ExtraDetails.ID("assimp/meshes/block/trapdoor/trap_door_bottom.fbx"));
+
+	@Override
+	public void render(BlockPos blockPos, ClientLevel level, PoseStack poseStack, MultiBufferSource bufferSource, float partialTick, int lightmapUV) {
+		poseStack.pushPose();
+		MatrixUtils.translateBlock(level.getBlockState(blockPos), poseStack);
+		MeshBlockRenderer.super.render(blockPos, level, poseStack, bufferSource, partialTick, lightmapUV);
+		poseStack.popPose();
+	}
+
+	@Override
+	public ResourceLocation getTexture(ClientLevel level, BlockPos blockPos, String textureName) {
+		BlockState blockState = level.getBlockState(blockPos);
+		ResourceLocation resourceLocation = blockState.getBlock().arch$registryName();
+		return resourceLocation.withPrefix("block/");
+	}
+
+	@Override
+	public Mesh getMesh(ClientLevel level, BlockPos blockPos) {
+		BlockState blockState = level.getBlockState(blockPos);
+		boolean isTop = blockState.getValue(TrapDoorBlock.HALF).equals(Half.TOP);
+		return isTop ? topMesh : bottomMesh;
+	}
+
+	@Override
+	public AnimationController<TrapDoorData> getAnimationController(ClientLevel level, BlockPos blockPos) {
+		return this;
+	}
+
+	@Override
+	public TrapDoorData getData(ClientLevel level, BlockPos blockPos) {
+		return level.extraDetails$getBlockData(blockPos, TrapDoorData::new);
+	}
+
+	@Override
+	public State registerStates(TrapDoorData trapDoorData) {
+		boolean isTop = trapDoorData.getBlockstate().getValue(TrapDoorBlock.HALF).equals(Half.TOP);
+		State offState = new AnimationState(ExtraDetails.ID(isTop ?
+				"assimp/animations/block/trapdoor/trap_door_close_top.fbx" : "assimp/animations/block/trapdoor/trap_door_close_bottom.fbx"));
+		State onState = new AnimationState(ExtraDetails.ID(isTop ?
+				"assimp/animations/block/trapdoor/trap_door_open_top.fbx" : "assimp/animations/block/trapdoor/trap_door_open_bottom.fbx"));
+
+		offState.nextTransitionState(() -> trapDoorData.getBlockstate().getValue(TrapDoorBlock.OPEN), onState, .1f);
+		onState.nextTransitionState(() -> !trapDoorData.getBlockstate().getValue(TrapDoorBlock.OPEN), offState, .1f);
+
+		return trapDoorData.getBlockstate().getValue(TrapDoorBlock.OPEN) ? onState : offState;
+	}
+
+	public static class TrapDoorData extends BlockData implements Animatable {
+		private final AnimatableInstance animatableInstance = new AnimatableInstance(this);
+
+		public TrapDoorData(BlockPos blockPos, Level level) {
+			super(blockPos, level);
+		}
+
+		@Override
+		public AnimatableInstance getAnimatableInstance() {
+			return this.animatableInstance;
+		}
+
+		@Override
+		public float getTick() {
+			return (float) (Blaze3D.getTime() * 20);
+		}
+	}
+}
